8ed6c15a2e497043c55cbe6a0854ce4ca448cc42
Eswar M | Dvara E-Registry <Eswar.M@Dvara.com>
2022-01-15 17:15:42 +0530

    Added static

---
+++ main/static/assets/js/main-new.js
@@ -0,0 +1,121 @@
+/**
+* Template Name: Medilab - v4.5.0
+* Template URL: https://bootstrapmade.com/medilab-free-medical-bootstrap-theme/
+* Author: BootstrapMade.com
+* License: https://bootstrapmade.com/license/
+*/
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = $('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+     
+    });
+    $(window).load(function() {
+ preloader.remove();
+    })
+  }
+
+  /**
+   * Initiate glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Initiate Gallery Lightbox 
+   */
+  const galelryLightbox = GLightbox({
+    selector: '.galelry-lightbox'
+  });
+  $(document).ready(function() {
+    $('.st-holder .nav-button').click(function() {
+      $(this).toggleClass('active');
+      $('.nav-holder').toggleClass('active');
+    });
+    $('.dropdown-toggle').click(function() {
+      $('ul.dropdown-menu.active').not($(this).next()).removeClass('active');
+      $('.dropdown-toggle.active').not(this).removeClass('active');
+      $(this).toggleClass('active');
+      $(this).next().toggleClass('active');
+    });
+
+    var cw = $('.img_gallery img').width();
+    $('.img_gallery img').css({'height':cw+'px'});
+  });
+
+
+
+
+
+})()
