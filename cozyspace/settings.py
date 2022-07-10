5562a7915f74a66586d26e5cbd9d837f50288a92
Eswar M | Dvara E-Registry <Eswar.M@Dvara.com>
2022-07-10 13:32:00 +0530

    Updated contactus

--- cozyspace/settings.py
+++ cozyspace/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'q@uv97(@q7r78z*+!*wfqe#@zrlsj1&_$dt-r=*^&h2_k8f%l3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
